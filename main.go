3cee757b60bfd48098aafae879842c48e4d001fb
antonio <antonio.belotti@studenti.unimi.it>
2020-12-21 19:32:26 +0100

    refactor main.go. port is now always 8080

--- main.go
+++ main.go
@@ -1,18 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-)
-
 func main() {
-	port, success := os.LookupEnv("EXPOSE_PORT")
-	if !success {
-		log.Println("Unable to read EXPOSE_PORT environment variable. Defaulting to port 8080")
-		port = "8080"
-	}
-
 	app := App{router: SetUpRouter()}
-	app.Run(fmt.Sprintf(":%s", port))
+	app.Run(":8080")
 }
