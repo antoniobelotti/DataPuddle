7cdee2f5914bef55a1ff649e808f540d3abf6ad2
antonio <antonio.belotti@studenti.unimi.it>
2020-12-22 13:12:32 +0100

    refactor tests

--- handlers_test.go
+++ handlers_test.go
@@ -9,9 +9,22 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 )
 
 const BASEURL = "http://localhost:8080/"
+
+func RequestNewKey() (string, error) {
+	resp, err := resty.New().R().Get("http://localhost:8080/sessionkey")
+	if err != nil {
+		return "", err
+	}
+
+	var jsonResponse SessionKeyReponse
+	json.Unmarshal(resp.Body(), &jsonResponse)
+
+	return jsonResponse.Key, nil
+}
 
 func init() {
 	app := App{router: SetUpRouter()}
@@ -21,14 +34,18 @@ func init() {
 type DataPuddleTestSuite struct {
 	suite.Suite
 	ApiClient *resty.Client
-	apikey    string
 }
 
 func (suite *DataPuddleTestSuite) SetupTest() {
 	suite.ApiClient = resty.New()
 	key, err := RequestNewKey()
 	assert.Nil(suite.T(), err)
-	suite.apikey = key
+
+	suite.ApiClient.SetTimeout(1 * time.Minute)
+	suite.ApiClient.SetHostURL(BASEURL)
+	suite.ApiClient.SetHeader("Accept", "application/json")
+	suite.ApiClient.SetHeader("Content-Type", "application/json")
+	suite.ApiClient.SetQueryParam("key", key)
 }
 
 func TestSuite(t *testing.T) {
@@ -50,7 +67,7 @@ func (suite *DataPuddleTestSuite) Test_IndexReturns_200_ok() {
 }
 
 func (suite *DataPuddleTestSuite) Test_PWDWorksWithExistingKey() {
-	response, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", suite.apikey))
+	response, _ := suite.ApiClient.R().Get("pwd")
 	var jsonResponse PWDResponse
 	json.Unmarshal(response.Body(), &jsonResponse)
 
@@ -60,7 +77,7 @@ func (suite *DataPuddleTestSuite) Test_PWDWorksWithExistingKey() {
 
 func (suite *DataPuddleTestSuite) Test_PWDFailsWithNonExistingKey() {
 	key := "scusasetelodicomaseimoltoitaliano"
-	response, err := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", key))
+	response, err := suite.ApiClient.R().SetQueryParam("key", key).Get("pwd")
 	assert.Nil(suite.T(), err)
 	var jsonResponse PWDResponse
 	json.Unmarshal(response.Body(), &jsonResponse)
@@ -69,25 +86,14 @@ func (suite *DataPuddleTestSuite) Test_PWDFailsWithNonExistingKey() {
 	assert.Equal(suite.T(), "", jsonResponse.Path)
 }
 
-func RequestNewKey() (string, error) {
-	resp, err := resty.New().R().Get("http://localhost:8080/sessionkey")
-	if err != nil {
-		return "", err
-	}
-
-	var jsonResponse SessionKeyReponse
-	json.Unmarshal(resp.Body(), &jsonResponse)
-
-	return jsonResponse.Key, nil
-}
-
 func (suite *DataPuddleTestSuite) Test_CDIntoOneSubdir() {
-	CDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test/"))
+	CDResp, _ := suite.ApiClient.R().SetQueryParam("path", "test/").Get("cd")
+
 	var jsonCDResp OutcomeResponse
 	json.Unmarshal(CDResp.Body(), &jsonCDResp)
 	assert.Equal(suite.T(), "ok", jsonCDResp.Outcome)
 
-	PWDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", suite.apikey))
+	PWDResp, _ := suite.ApiClient.R().Get("pwd")
 	var jsonResponse PWDResponse
 	json.Unmarshal(PWDResp.Body(), &jsonResponse)
 
@@ -96,12 +102,12 @@ func (suite *DataPuddleTestSuite) Test_CDIntoOneSubdir() {
 }
 
 func (suite *DataPuddleTestSuite) Test_CDIntoManySubdir() {
-	CDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test/sub/user"))
+	CDResp, _ := suite.ApiClient.R().SetQueryParam("path", "test/sub/user").Get("cd")
 	var jsonCDResp OutcomeResponse
 	json.Unmarshal(CDResp.Body(), &jsonCDResp)
 	assert.Equal(suite.T(), "ok", jsonCDResp.Outcome)
 
-	PWDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", suite.apikey))
+	PWDResp, _ := suite.ApiClient.R().Get("pwd")
 	var jsonResponse PWDResponse
 	json.Unmarshal(PWDResp.Body(), &jsonResponse)
 
@@ -110,17 +116,17 @@ func (suite *DataPuddleTestSuite) Test_CDIntoManySubdir() {
 }
 
 func (suite *DataPuddleTestSuite) Test_CDToRoot() {
-	CDResp1, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test/sub/"))
+	CDResp1, _ := suite.ApiClient.R().SetQueryParam("path", "test/sub/").Get("cd")
 	var jsonCDResp1 OutcomeResponse
 	json.Unmarshal(CDResp1.Body(), &jsonCDResp1)
 	assert.Equal(suite.T(), "ok", jsonCDResp1.Outcome)
 
-	CDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "/"))
+	CDResp, _ := suite.ApiClient.R().SetQueryParam("path", "/").Get("cd")
 	var jsonCDResp OutcomeResponse
 	json.Unmarshal(CDResp.Body(), &jsonCDResp)
 	assert.Equal(suite.T(), "ok", jsonCDResp.Outcome)
 
-	PWDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", suite.apikey))
+	PWDResp, _ := suite.ApiClient.R().Get("pwd")
 	var jsonResponse PWDResponse
 	json.Unmarshal(PWDResp.Body(), &jsonResponse)
 
@@ -130,17 +136,17 @@ func (suite *DataPuddleTestSuite) Test_CDToRoot() {
 
 func (suite *DataPuddleTestSuite) Test_CDDotDot() {
 
-	CDResp1, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test/sub/"))
+	CDResp1, _ := suite.ApiClient.R().SetQueryParam("path", "test/sub/").Get("cd")
 	var jsonCDResp1 OutcomeResponse
 	json.Unmarshal(CDResp1.Body(), &jsonCDResp1)
 	assert.Equal(suite.T(), "ok", jsonCDResp1.Outcome)
 
-	CDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, ".."))
+	CDResp, _ := suite.ApiClient.R().SetQueryParam("path", "..").Get("cd")
 	var jsonCDResp OutcomeResponse
 	json.Unmarshal(CDResp.Body(), &jsonCDResp)
 	assert.Equal(suite.T(), "ok", jsonCDResp.Outcome)
 
-	PWDResp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s", BASEURL, "pwd", suite.apikey))
+	PWDResp, _ := suite.ApiClient.R().Get("pwd")
 	var jsonResponse PWDResponse
 	json.Unmarshal(PWDResp.Body(), &jsonResponse)
 
@@ -149,7 +155,7 @@ func (suite *DataPuddleTestSuite) Test_CDDotDot() {
 }
 
 func (suite *DataPuddleTestSuite) Test_MKDIRSuccess() {
-	resp, _ := suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "mkdir", suite.apikey, "test/sub/"))
+	resp, _ := suite.ApiClient.R().SetQueryParam("path", "test/sub/").Get("mkdir")
 	var jsonResponse OutcomeResponse
 	json.Unmarshal(resp.Body(), &jsonResponse)
 
@@ -163,11 +169,12 @@ func (suite *DataPuddleTestSuite) Test_STORESuccessful() {
 	file, err := ioutil.ReadFile("fixtures/testbig.json")
 	assert.Nil(suite.T(), err)
 
-	suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test"))
+	suite.ApiClient.R().SetQueryParam("path", "test/").Get("cd")
 
 	resp, _ := suite.ApiClient.R().
 		SetBody(file).
-		Post(fmt.Sprintf("%s%s?key=%s&filename=%s", BASEURL, "store", suite.apikey, "big.json"))
+		SetQueryParam("filename", "big.json").
+		Post("store")
 	var jsonResponse OutcomeResponse
 	json.Unmarshal(resp.Body(), &jsonResponse)
 
@@ -175,17 +182,17 @@ func (suite *DataPuddleTestSuite) Test_STORESuccessful() {
 }
 
 func (suite *DataPuddleTestSuite) Test_STOREFailsIfFileAlreadyExists() {
-	suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test"))
-
-	suite.ApiClient.R().Get(fmt.Sprintf("%s/%s?key=%s&path=%s", BASEURL, "cd", suite.apikey, "test"))
+	suite.ApiClient.R().SetQueryParam("path", "test/sub/").Get("cd")
 
 	suite.ApiClient.R().
 		SetBody(`{"username":"testuser"}`).
-		Post(fmt.Sprintf("%s%s?key=%s&filename=%s", BASEURL, "store", suite.apikey, "fail.json"))
+		SetQueryParam("filename", "fail.json").
+		Post("store")
 
 	resp, _ := suite.ApiClient.R().
 		SetBody(`{"username":"testuser"}`).
-		Post(fmt.Sprintf("%s%s?key=%s&filename=%s", BASEURL, "store", suite.apikey, "fail.json"))
+		SetQueryParam("filename", "fail.json").
+		Post("store")
 	var jsonResponse OutcomeResponse
 	json.Unmarshal(resp.Body(), &jsonResponse)
 
